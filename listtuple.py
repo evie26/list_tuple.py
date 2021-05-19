85eff0fa9dc38420f83e40471c89bfa14d5ef9c7
Eva Hancocks <82679886+evie26@users.noreply.github.com>
2021-05-19 19:25:20 +0100

    Add files via upload

---
+++ listtuple.py
@@ -0,0 +1,29 @@
+# Example of list_tuple dictionary
+# List example, must be use with brackets
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "orange"
+
+# Example of tuple must be use with parenthesis
+print(myFruitList)
+myFinalAnswerTuple = ("pen", "pineapple", "pens")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+myFavoriteFruitDictionary = {
+    "Adam": "pen",
+    "Ben": "pineapple",
+    "Penny": "pens"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Adam"])
+print(myFavoriteFruitDictionary["Ben"])
+print(myFavoriteFruitDictionary["Pens"])
